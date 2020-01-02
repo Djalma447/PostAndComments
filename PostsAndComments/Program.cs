202ac6bc20376e99bc9debdc29f07fcd10ef90df
Djalma <djalma.ant@gmail.com>
2020-01-02 01:56:10 -0300

    Creating the Project

---
+++ PostsAndComments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostsAndComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
